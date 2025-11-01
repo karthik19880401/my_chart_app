a57a3cf3186c2f95764a0ecb6cc1c58e2dc3f153
Karthik <karthikparthasarathy1988@gmail.com>
2025-11-02 00:23:56 +0100

    First version of my_chart_app

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Chart App (Vite + React + Tailwind + Recharts)
+
+Quick start:
+1. unzip and cd into folder
+2. npm install
+3. npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LinearGraph from "./LinearGraph";
+
+export default function App() {
+  return (
+    <div className="flex flex-wrap justify-center gap-8 p-8">
+      <LinearGraph />
+     
+    </div>
+  );
+}
+
